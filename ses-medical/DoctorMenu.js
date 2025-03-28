4211602d9dbbc7fcf3682e53b6fb97e22331cc87
Bamzy123 <stephenomotos@gmail.com>
2025-03-28 13:51:56 +0100

    DoctorMenu

---
+++ ses-medical/DoctorMenu.js
@@ -0,0 +1,21 @@
+// components/DoctorMenu.js
+import React from "react";
+
+function DoctorMenu() {
+  return (
+    <div>
+      <h2>Welcome, Doctor</h2>
+      <ul>
+        <li>View Patients Medical Records</li>
+        <li>Treatment Plans</li>
+        <li>Available Prescriptions</li>
+        <li>Update Patient Medical Record</li>
+        <li>Generate Report</li>
+        <li>Appointments</li>
+        <li>Emergency Contacts</li>
+      </ul>
+    </div>
+  );
+}
+
+export default DoctorMenu;
