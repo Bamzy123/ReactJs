431601cbf22d9073b383a25b337ea6806e025543
Bamzy123 <stephenomotos@gmail.com>
2025-03-28 13:51:06 +0100

    App

---
+++ ses-medical/App.js
@@ -0,0 +1,20 @@
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import DoctorMenu from "./components/DoctorMenu";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/doctor-menu" element={<DoctorMenu />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
